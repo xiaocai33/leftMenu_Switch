e88de364818d97a0a11ec5448a3e4eb04c31f2fa
xiaocai <tycai1@sina.com>
2016-06-05 12:27:25 +0800

    初始化控制器完成

---
+++ XCLeftMenuSwitch/XCLeftMenuSwitch/Classes/Controller/MainViewController.m
@@ -0,0 +1,19 @@
+//
+//  MainViewController.m
+//  XCLeftMenuSwitch
+//
+//  Created by 小蔡 on 16/6/5.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+@end
---
+++ XCLeftMenuSwitch/XCLeftMenuSwitch/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XCLeftMenuSwitch
+//
+//  Created by 小蔡 on 16/6/5.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
